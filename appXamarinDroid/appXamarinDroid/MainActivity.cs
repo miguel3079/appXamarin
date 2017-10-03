8c9b80c288a510a7b1f041267968a673c008af1f
Miguel3079 <miguel.informa@gmail.com>
2017-10-03 16:07:51 +0200

    Initial Commint

---
+++ appXamarinDroid/appXamarinDroid/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+using appXamarinDroid.Model;
+
+namespace appXamarinDroid
+{
+    [Activity(Label = "appXamarinDroid", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            //Initializing button from layout
+            Button login = FindViewById<Button>(Resource.Id.login);
+
+            //Login button click action
+            login.Click += (object sender, EventArgs e) => {
+                Connection connection = new Connection();
+                Android.Widget.Toast.MakeText(this, connection.GetAccountCountFromMySQL(), Android.Widget.ToastLength.Short).Show();
+            };
+        }
+    }
+}
+
---
+++ appXamarinDroid/appXamarinDroid/Model/Connection.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MySql.Data.MySqlClient;
+
+namespace appXamarinDroid.Model
+{
+    public class Connection
+    {
+        private string GetAccountCountFromMySQL()
+        {
+            try
+            {
+                MySqlConnection sqlconn;
+                string connsqlstring = "Server=your.ip.address;Port=3306;database=YOUR_DATA_BASE;User Id=root;Password=password;charset=utf8";
+                sqlconn = new MySqlConnection(connsqlstring);
+                sqlconn.Open();
+                string queryString = "select count(0) from ACCOUNT";
+                MySqlCommand sqlcmd = new MySqlCommand(queryString, sqlconn);
+                String result = sqlcmd.ExecuteScalar().ToString();
+                return "conectado";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+        public bool Login(string user, string pass)
+        {
+            return true;
+        }
+    }
+}
